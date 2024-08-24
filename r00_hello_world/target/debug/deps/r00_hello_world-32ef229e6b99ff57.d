02e11f4f400f66739d046dacc4702298a1b1134d
Abhinay Jangde <abhinayjangde@gmail.com>
2024-08-24 18:44:37 +0530

    r00_hello_world

---
+++ r00_hello_world/target/debug/deps/r00_hello_world-32ef229e6b99ff57.d
@@ -0,0 +1,5 @@
+D:\Rust\r00_hello_world\target\debug\deps\r00_hello_world-32ef229e6b99ff57.exe: src/main.rs
+
+D:\Rust\r00_hello_world\target\debug\deps\r00_hello_world-32ef229e6b99ff57.d: src/main.rs
+
+src/main.rs:
---
+++ r00_hello_world/target/debug/r00_hello_world.d
@@ -0,0 +1,1 @@
+D:\Rust\r00_hello_world\target\debug\r00_hello_world.exe: D:\Rust\r00_hello_world\src\main.rs
